54cd973a2b17d589fbad70dc771bd9d979faa1b3
fengnanyue <fengnanyue@zju.edu.cn>
2015-09-12 23:33:02 -0600

    Http Post

---
+++ HttpPost/app/src/main/java/com/fengnanyue/httppost/MainActivity.java
@@ -0,0 +1,73 @@
+package com.fengnanyue.httppost;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btnRead).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AsyncTask<String,Void,Void>(){
+
+                    @Override
+                    protected Void doInBackground(String... params) {
+                        try {
+                            URL url = new URL(params[0]);
+                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+
+                            connection.setDoOutput(true);
+                            connection.setRequestMethod("POST");
+
+                            OutputStreamWriter osw = new OutputStreamWriter(connection.getOutputStream(),"utf-8");
+                            BufferedWriter bw = new BufferedWriter(osw);
+                            bw.write("keyfrom=testHttpGet10&key=826938331&type=data&doctype=xml&version=1.1&q=good");
+                            bw.flush();
+
+
+
+                            InputStream is = connection.getInputStream();
+                            InputStreamReader isr = new InputStreamReader(is,"utf-8");
+                            BufferedReader br = new BufferedReader(isr);
+                            String line;
+                            while((line = br.readLine())!=null){
+                                System.out.println(line);
+                            }
+                            br.close();
+                            isr.close();
+                            is.close();
+
+
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                }.execute( "http://fanyi.youdao.com/openapi.do" );
+
+            }
+        });
+    }
+
+
+
+}
